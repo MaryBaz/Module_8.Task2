4dc962537f62574db7b47443c54d9f57ca245f22
Name <email@example.ru>
2024-07-24 20:57:34 +0500

    Добавьте файлы проекта.

---
+++ Module_8.Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Module_8.Task2
+{
+    public static class DirExt
+    {
+        public static long FolderSize(this DirectoryInfo folder)
+        {
+            long size = 0;
+            try
+            {
+                FileInfo[] files = folder.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    size += file.Length;
+                }
+
+                foreach (DirectoryInfo directory in folder.GetDirectories())
+                {
+                    //Console.ForegroundColor = ConsoleColor.Cyan;
+                    //Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"Размер : {directory} = {size} байт");
+                    size += FolderSize(directory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            return size;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Укажите директорию: ");
+            string? DirPath = Console.ReadLine();
+            //string DirPath = @"D:\";
+            if (Directory.Exists(DirPath))
+            {
+                //Console.ForegroundColor = ConsoleColor.Cyan;
+                //Console.WriteLine($"Директория {DirPath}");
+                DirectoryInfo dir = new DirectoryInfo(DirPath);
+                //dir.FolderSize();
+                //Console.ForegroundColor = ConsoleColor.Cyan;
+                //Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine($"Размер директории {DirPath} = {dir.FolderSize()} байт");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Директории {DirPath} не найдено!");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+
+        }
+    }
+}
